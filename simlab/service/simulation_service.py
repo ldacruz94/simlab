4bb7557b7207b4c436d76d619f163285e74bac3e
Lucas <ldacru1@wgu.edu>
2026-01-22 21:50:59 -0500

    resolve lint warnings and update ci

--- simlab/service/simulation_service.py
+++ simlab/service/simulation_service.py
@@ -1,3 +1,8 @@
+"""
+api/simulation_service.py
+
+Defines the service methods used in the API routes for modifying the db
+"""
 
 from fastapi import HTTPException
 from simlab.db.models import SimulationRun, RunResponseDTO, RunRequestDTO, RunResponseListDTO
@@ -5,6 +10,11 @@ from simlab.db.session import get_db_session
 
 
 async def create_run(run_request: RunRequestDTO) -> HTTPException | None:
+    """
+    Create a simulation run
+    :param run_request:
+    :return: HTTPException if an error is present
+    """
     try:
         with get_db_session() as session:
             new_run = SimulationRun(
@@ -17,9 +27,14 @@ async def create_run(run_request: RunRequestDTO) -> HTTPException | None:
             session.refresh(new_run)
     except Exception as ex:
         print(f"Could not create simulation run: {ex}")
-        raise HTTPException(status_code=500, detail="Failed to create simulation run")
+        raise HTTPException(status_code=500, detail="Failed to create simulation run") from ex
 
 async def get_simulation_run(run_id: int) -> RunResponseDTO | HTTPException:
+    """
+    Get a simulation run by ID
+    :param run_id: Simulation Run ID
+    :return: RunResponse or HTTPException if an error is present
+    """
     try:
         with get_db_session() as session:
             result: SimulationRun | None = (
@@ -37,9 +52,13 @@ async def get_simulation_run(run_id: int) -> RunResponseDTO | HTTPException:
         raise
     except Exception as ex:
         print(f"Simulation run not found: {ex}")
-        raise HTTPException(status_code=500, detail="Failed to find run")
+        raise HTTPException(status_code=500, detail="Failed to find run") from ex
 
 async def get_simulation_runs() -> RunResponseListDTO:
+    """
+    Get a list of simulation runs
+    :return: List of Simulation Runs
+    """
     try:
         with get_db_session() as session:
             results = session.query(SimulationRun).all()
@@ -55,4 +74,4 @@ async def get_simulation_runs() -> RunResponseListDTO:
         raise
     except Exception as ex:
         print(f"Simulation run not found: {ex}")
-        raise HTTPException(status_code=500, detail="Failed to find run")
+        raise HTTPException(status_code=500, detail="Failed to find run") from ex
