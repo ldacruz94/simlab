23acbca1207504d9d7353d6484de3b957ad2e194
Lucas <ldacru1@wgu.edu>
2026-01-25 06:53:23 -0500

    add rabbitmq and celery

---
+++ simlab/core/celery_app.py
@@ -0,0 +1,13 @@
+import os
+
+from celery import Celery
+
+celery_app = Celery(
+    "simlab",
+    broker=os.getenv("RABBITMQ_URL"),
+    backend=os.getenv("CELERY_RESULT_BACKEND")
+)
+
+celery_app.conf.task_routes = {
+    "simlab.tasks.simulation_tasks.run_simulation": {"queue": "simulation_queue"},
+}
