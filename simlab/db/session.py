2e951143450b99243f64c270e07cc94471799b6f
Lucas Da Cruz <lucaspavesi11@gmail.com>
2026-01-25 07:52:29 -0500

    add pre-commit
    test pre-commit
    
    test pre-commit lint
    
    final test pre-commit

--- simlab/db/session.py
+++ simlab/db/session.py
@@ -11,7 +11,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
